83339743e9fc413555779f84fb67bc0f77e3aad1
laugh <laugh@email.com>
2023-06-17 17:13:07 +0800

    集成 gin

--- mian.go
+++ mian.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello wdf!!")
+	// new 一个 Gin Engine 实例
+	r := gin.Default()
+	// 注册中间件
+	r.Use(gin.Logger(), gin.Recovery())
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "wdf",
+		})
+	})
+	// 处理 404 请求
+	r.NoRoute(func(c *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+
+	r.Run(":8066")
 }
