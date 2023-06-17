16335019727609ad3460e231b464ae56eb14d47b
laugh <laugh@email.com>
2023-06-17 18:11:16 +0800

    初始化路由

--- mian.go
+++ mian.go
@@ -1,37 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
+	"gohub/bootstrap"
 )
 
 func main() {
 	// new 一个 Gin Engine 实例
-	r := gin.Default()
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "wdf",
-		})
-	})
-	// 处理 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
+	r := gin.New()
+	// 初始化路由绑定
+	bootstrap.SetupRoute(r)
 
-	r.Run(":8066")
+	err := r.Run(":8066")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
