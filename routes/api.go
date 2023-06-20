7acf7e3c36b613b307c6c57a774a0bc0e0bbd335
laugh <laugh@email.com>
2023-06-20 22:31:47 +0800

    图片验证码接口

--- routes/api.go
+++ routes/api.go
@@ -17,6 +17,9 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			// 判断手机是否已注册
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+
+			vcc := new(auth.VerifyCodeController)
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 		}
 	}
 }
