48133299936edb13a50f3ef84d828b174567973f
laugh <laugh@email.com>
2023-06-23 12:22:36 +0800

    feat:创建console包来显示命令行的打印信息设置

--- routes/api.go
+++ routes/api.go
@@ -6,14 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// RegisterAPIRoutes 注册网页相关路由
+// RegisterAPIRoutes 注册 API 相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
 	v1.Use(middlewares.LimitIP("200-H"))
+
 	{
 		authGroup := v1.Group("/auth")
+		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+		// 测试时，可以调高一点
 		authGroup.Use(middlewares.LimitIP("1000-H"))
 		{
 			// 登录
